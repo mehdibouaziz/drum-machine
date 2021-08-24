11078b054316c8a483f8a90c534472a32107d18a
Mehdi Bouaziz <mehdi.bouaziz@gadz.org>
2021-08-24 19:03:18 +0200

    adding V1.0

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
 
-    // jQuery methods go here...
-    $(".btn-warning").attr("class", "btn btn-outline-warning drum-pad w-100 m-1 flex-grow-1");
-    $(".btn-primary").attr("class", "btn btn-outline-primary drum-pad w-100 m-1 flex-grow-1");
-    $(".btn-danger").attr("class", "btn btn-outline-danger drum-pad w-100 m-1 flex-grow-1");
+    $(".btn-warning").attr("class", "btn btn-outline-warning drum-pad w-100 m-1 flex-grow-1 fs-4");
+    $(".btn-primary").attr("class", "btn btn-outline-primary drum-pad w-100 m-1 flex-grow-1 fs-4");
+    $(".btn-danger").attr("class", "btn btn-outline-danger drum-pad w-100 m-1 flex-grow-1 fs-4");
 
-    $("#drum-machine").attr("class", 'd-flex flex-row');
+    $("#drum-machine").attr("class", 'd-flex flex-row justify-content-center align-items-center vw-100 vh-100');
+    $("#drum-machine-table").attr("class", 'd-flex flex-row');
     $("#keyboard-row").attr("class", 'keyboard-row h-100 d-flex flex-row justify-content-center align-items-center');
     $(".keyboard-col").attr("class", 'keyboard-col h-100 d-flex flex-column justify-content-center align-items-center flex-grow-1 m-1');
     $("#console").attr("class", 'd-flex flex-column justify-content-center align-items-center');
@@ -21,12 +21,17 @@ $(document).ready(function(){
 
     
     $("button.drum-pad").click(function(){
-      var ID = "#" + $(this).text();
-      console.log(ID);
+      let ID = "#" + $(this).text();
+      let t = $(this).attr("id");
       $(ID).currentTime = 0;
       $(ID).get(0).play();
+      $("#display").text(t);
     });
     
+    $(document).keydown(function(event){
+      let ID = "#" + String.fromCharCode(event.which);
+      $(ID).parent().click();
+    });
 
 
   });
