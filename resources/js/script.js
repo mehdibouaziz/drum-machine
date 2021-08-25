a6898abdf68bb05119727d9edf282848c3ccf020
Mehdi Bouaziz <mehdi.bouaziz@gadz.org>
2021-08-25 15:17:27 +0200

    backup

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,16 @@
 
 $(document).ready(function(){
 
-    $(".btn-warning").attr("class", "btn btn-outline-warning drum-pad w-100 m-1 flex-grow-1 fs-4");
-    $(".btn-primary").attr("class", "btn btn-outline-primary drum-pad w-100 m-1 flex-grow-1 fs-4");
-    $(".btn-danger").attr("class", "btn btn-outline-danger drum-pad w-100 m-1 flex-grow-1 fs-4");
+    $(".btn-warning").attr("class", "btn btn-outline-warning drum-pad h-100 m-1 fs-4");
+    $(".btn-primary").attr("class", "btn btn-outline-primary drum-pad h-100 m-1 fs-4");
+    $(".btn-danger").attr("class", "btn btn-outline-danger drum-pad h-100 m-1 fs-4");
 
     $("#drum-machine").attr("class", 'd-flex flex-row justify-content-center align-items-center vw-100 vh-100');
     $("#drum-machine-table").attr("class", 'd-flex flex-row');
-    $("#keyboard-row").attr("class", 'keyboard-row h-100 d-flex flex-row justify-content-center align-items-center');
-    $(".keyboard-col").attr("class", 'keyboard-col h-100 d-flex flex-column justify-content-center align-items-center flex-grow-1 m-1');
+
+    $(".keyboard-col").attr("class", 'keyboard-col h-100 d-flex flex-column justify-content-center align-items-center');
+    $(".keyboard-row").attr("class", 'keyboard-row w-100 h-100 d-flex flex-row justify-content-center align-items-center m-1');
+
     $("#console").attr("class", 'd-flex flex-column justify-content-center align-items-center');
     $("#display-box").attr("class", 'd-flex flex-row justify-content-center align-items-center w-75');
 
